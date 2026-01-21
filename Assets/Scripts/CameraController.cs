c979948a258d4301a305922114b69b44d53a859b
athirujpao <165152150+Athir0j@users.noreply.github.com>
2026-01-22 00:16:43 +0700

    add edge scrolling

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -10,10 +10,11 @@ public class CameraController : MonoBehaviour
     [SerializeField] private Transform corner2;
 
 
-
-
     [SerializeField] private float xInput;
     [SerializeField] private float zInput;
+
+    
+
     public static CameraController instance;
 
     private Vector3 Clamp(Vector3 lowerLeft, Vector3 topRight)
@@ -22,6 +23,41 @@ public class CameraController : MonoBehaviour
                                         transform.position.y,Mathf.Clamp(transform.position.z,lowerLeft.z, topRight.z));
         return pos;
     }
+
+    [Header("Zoom")]
+    [SerializeField] private float zoomModifier;
+    private void Zoom()
+    {
+        zoomModifier = Input.GetAxis("Mouse ScrollWheel");
+        if (Input.GetKey(KeyCode.Z))
+            zoomModifier = -0.2f;
+        if (Input.GetKey(KeyCode.X))
+            zoomModifier = 0.2f;
+
+        cam.orthographicSize += zoomModifier;
+        cam.orthographicSize = Mathf.Clamp(cam.orthographicSize, 4,10);
+    }
+
+    
+    private void MoveByMouse()
+    {
+        //ผมไม่แน่จะนำว่าทำไงไปซ้ายไปไงแต่จาก check debug ตำแหน่งเมาส์ เป็นเลยลองให้เป็น 0 ไปเลย แบบเดี๋ยวรอดูของ อาจารย์ วันเสาร์นี้ หวังว่าผมจะไปทันT-T 
+        Debug.Log(Input.mousePosition);
+        //right
+        if (Input.mousePosition.x >= Screen.width)
+            transform.Translate(Vector3.right * moveSpeed * Time.deltaTime, Space.World);
+        //left
+        if (Input.mousePosition.x <= 0)
+            transform.Translate(Vector3.left * moveSpeed * Time.deltaTime, Space.World); 
+        //up
+        if (Input.mousePosition.y >= Screen.height)
+            transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime, Space.World);
+        //down
+        if (Input.mousePosition.y <= 0)
+            transform.Translate(Vector3.back * moveSpeed * Time.deltaTime, Space.World);
+        
+    }
+    
 
     void Awake()
     {
@@ -51,5 +87,8 @@ public class CameraController : MonoBehaviour
     void Update()
     {
         MoveByKB();
+        Zoom();
+        MoveByMouse();
     }
+
 }
